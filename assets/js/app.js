6441025febee912ce7b2000812e5fc11c691ccc2
Ritesh Shrivastav <rtshshrivastav@gmail.com>
2015-02-18 13:14:44 +0530

    No cache.

--- assets/js/app.js
+++ assets/js/app.js
@@ -41,4 +41,18 @@ $(document).ready(function(){
 		var $affixElement = $('div.side-list');
 		$affixElement.width($affixElement.parent().width());
 	});
+
+	// Check if a new cache is available on page load.
+	window.addEventListener('load', function(e) {
+		window.applicationCache.addEventListener('updateready', function(e) {
+			if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+				// Browser downloaded a new app cache.
+				// Swap it in and reload the page to get the new hotness.
+				window.applicationCache.swapCache();
+				if (confirm('A new version of this site is available. Load it?')) {
+					window.location.reload();
+				}
+			}
+		}, false);
+	}, false);
 });
